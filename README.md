e61acacf008d382263145a1b6d5c0cba73c300d3
Tommy Ryan <tryan@qti.qualcomm.com>
2015-06-25 16:23:37 -0700

    Added some super basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # LikeAllTheThings
 A TamperMonkey script to like all the posts on your Facebook Wall 
+
+## Use with Caution!! ##
+This can probably get you banned from some Facebook features until I've experimented with the throttle a bit more
+
+## Installation ##
+**It is recommended to use either [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (Chrome)** This allows the script to auto-update to the most recent version. The rate of this update can be changed in each extension's preferences.
+
+### [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) ###
+
+1. Navigate to `https://raw.githubusercontent.com/tallboy/LikeAllTheThings/master/likeAllTheThings.js` or click [here](https://raw.githubusercontent.com/tallboy/LikeAllTheThings/master/likeAllTheThings.js).
+2. When the editor has loaded, click `Install` (*NOT* `Process with Chrome`).
+
+###TODOs
+* Add a UI to start/stop
+* Scroll along with the clicks
+* Incorporate tag based liking (whitelist / blacklist)
---
+++ likeAllTheThings.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         LikeAllTheThings
+// @namespace    http://www.tommy-ryan.com
+// @version      0.1
+// @description  A simple script to like all the posts your friends make
+// @author       TALLBOY
+// @match        https://www.facebook.com/
+// @grant        none
+// ==/UserScript==
+
+
+(function(w) {
+	"use strict";
+
+	var mainContain = document.getElementById('mainContainer');
+	var offset = mainContain.offsetTop;
+	
+	function getLikeButtons() {
+		var likes = document.querySelectorAll('.UFILikeLink');
+		console.dir(likes);
+
+		return likes;
+	};
+	
+	function scrollToNextLike(like) {
+		var likeOffset = like.offsetTop;
+		mainContain.scrollTo = likeOffset;
+		return true;
+	};
+	
+	function processLikes() {
+		var likes = getLikeButtons();
+		for (var i=0; i < likes.length; i++) {
+			//like['title'] = 'Like this'
+			//like['title'] = 'Unlike this'
+			if (likes[i]['title'] == 'Like this') {
+				console.log('Like me!', likes[i]);
+				// Throttle the clicks a bit so we don't get banned from Facebook
+				setTimeout(function() {
+					likes[i].click();
+				}, 1000);
+			};
+			scrollToNextLike(likes[i]);
+		};
+	};
+
+	processLikes();
+	setInterval(processLikes, 5000);
+
+}(window));
+
